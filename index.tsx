d95077201feb546680d06d67a314d04de2463211
Nizhad Bayramov <nizhad.bayramov@kaspersky.com>
2025-11-20 02:04:53 +0700

    fix: remove invalid css import causing build failure

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { LanguageProvider } from './contexts/LanguageContext';
-import './index.css'; // Assuming there might be a global css file, adding it just in case if it was implicit or forgot in previous reads. If not, I will remove it. Wait, checking project layout again. index.html exists. Usually css is imported. I'll stick to minimal changes.
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
